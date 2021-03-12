b02ba04c91e8c1613373b3ad3d0bb1f1061e4e1d
82104 <82104@DESKTOP-D8PJ142>
2021-03-12 17:15:04 +0900

    수정

--- src/mvc/model/dao/SupervisorDAO.java
+++ src/mvc/model/dao/SupervisorDAO.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 import mvc.model.dto.Supervisor;
 
 public interface SupervisorDAO {
-	/**
+	/** 최훈석
 	 * 관리자로 로그인하기
 	 * */
 	Supervisor login(String svId, int password)throws SQLException;
