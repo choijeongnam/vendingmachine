cf66adca5d87a62aa6818737cae1f71ddecc9580
woo <uusuu1230@gmail.com>
2021-03-15 18:06:42 +0900

    수정

--- src/mvc/view/EndView.java
+++ src/mvc/view/EndView.java
@@ -18,8 +18,8 @@ public class EndView {
 		
 		System.out.println("자판기번호 | 메뉴코드 | 재고 수");
 		for(Goods goods :list) {
-			System.out.print("   "+goods.getVmNo()+"\t");
-			System.out.print("\t"+goods.getMenuCode()+"\t");
+			System.out.print("   "+goods.getVmNo()+"     |");
+			System.out.print("    "+goods.getMenuCode()+"   |");
 			System.out.println("   "+goods.getStock());
 		}
 		System.out.println();
